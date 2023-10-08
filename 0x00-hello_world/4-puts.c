91f3167ab8fd115138d48e1dcc5bbfcc630ab320
jayludis <jayludis@gmail.com>
2023-10-08 15:11:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main * this is the main function
+ * main - this is the main function
  * Return always 0
  */
 int main(void)
 {
+	/*this line prints out a string */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
