5f1b3d60d762a54fed1f70515f21e5bc240c12ec
jayludis <jayludis@gmail.com>
2023-10-16 15:26:16 -0700

    1

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,5 +15,7 @@ void print_array(int *a, int n)
 	{
 		if (i == 0)										printf("%d", a[i]);
 		else
-			printf(", %d", a[i])						}	printf("\n");
+			printf(", %d", a[i]);
+	}
+		printf("\n");
 }
