8984a4a0831fe413b20d6d5384f6af97dc7960c9
jayludis <jayludis@gmail.com>
2024-06-26 22:14:22 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,20 +3,19 @@
 /*
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
+ * Return 0
  */
+
 void print_alphabet_x10(void)
 {
 
-	char c;
-	int i = 0;
+	int i;
+	char j;
 
-	while (i <= 9)
+	for (i = '0'; i < '10'; i++) 
 	{
-	for (c = 'a'; c <= 'z'); c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	i++;
+		for (j = 'a' ; j <= 'z'; j++
+			_putchar(j);
+		_putchar('\n');
 	}
 }
