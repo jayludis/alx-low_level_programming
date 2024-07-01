6072a8a0b94a1956f68374b113957b4e578719dd
jayludis <jayludis@gmail.com>
2024-07-01 16:33:32 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,6 +8,7 @@
 void puts2(char *str)
 {
 	int len, i;
+	
 	len = 0;
 	while (str[len] != '\0')
 	{
@@ -15,7 +16,7 @@ void puts2(char *str)
 	}
 	for (i = 0; i < len; i += 2)
 	{
-		_putchar(str[i]);  				
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
