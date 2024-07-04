e6c391ca37733787cfe87d3234e4512b0dee67b7
jayludis <jayludis@gmail.com>
2024-07-04 14:33:18 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return Always 0.
+ */
+int man(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_puchar('\n');
+	print_number(1024);
+	_puchar('\n');
+	print_number(0);
+	_puchar('\n');
+	print_number(-98);
+	_puchar('\n');
+	return (0);
+}
