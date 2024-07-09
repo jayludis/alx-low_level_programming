003b3204d6bb75ad057c7240dcc749b335aaec67
jayludis <jayludis@gmail.com>
2024-07-09 12:54:33 -0700

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * _strchr - prints from the first occurrence of a char.
- * @s: source string
- * @c: tested char
- *  
- * Return: new string.
+ *@s: source string
+ *@c: tested char
+ *Return: new string.
  */
 char *_strchr(char *s, char c)
 {
