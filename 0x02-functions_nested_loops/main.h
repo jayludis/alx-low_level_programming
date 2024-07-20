2dab54ce52565bf928c6c8b886344e3b296b66f9
jayludis <jayludis@gmail.com>
2024-07-20 14:47:07 +0000

    Correct function prototype typo

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void limes_table(void);
+void times_table(void);
 void print_to_98(int n);
 void print_times_table(int n);
 
