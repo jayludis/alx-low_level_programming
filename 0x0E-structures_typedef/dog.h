33c883ec46ecadf54b0e81c3bced725badb9efc0
jayludis <jayludis@gmail.com>
2024-07-24 17:49:31 -0700

    all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,6 +9,7 @@
 *
 * Description: Longer description
 */
+
 struct dog
 {
 	char *name;
@@ -16,4 +17,10 @@ struct dog
 	char *owner;
 };
 
-#endif
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
